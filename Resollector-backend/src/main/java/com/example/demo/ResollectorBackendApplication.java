53c0e8c0ba1290aef788aa2bdb7f5b4239d33808
sikakujira <sikakujira@gmail.com>
2024-03-20 00:34:35 +0900

    first commit

---
+++ Resollector-backend/src/main/java/com/example/demo/ResollectorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResollectorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResollectorBackendApplication.class, args);
+	}
+
+}
